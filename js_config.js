b43f10bedca5b8fa5f739ab61c2b4ea894bf1f14
Tanansh-Ahuja <tananshahuja17@gmail.com>
2025-05-10 05:14:14 +0530

    js config production

--- js_config.js
+++ js_config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000"; //development
-//export const BASE_URL = "pool-party-roorkee-backend.railway.internal"; //production
+//export const BASE_URL = "http://localhost:8000"; //development
+export const BASE_URL = "pool-party-roorkee-backend.railway.internal"; //production
