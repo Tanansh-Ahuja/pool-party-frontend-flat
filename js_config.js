283a3a8f9ba5fcac1e1bff7c7247625ccc69d764
Tanansh-Ahuja <tananshahuja17@gmail.com>
2025-05-15 23:54:16 +0530

    js config fix

--- js_config.js
+++ js_config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000"; //development
-//export const BASE_URL = "https://api.poolpartyroorkee.in"; //production
+//export const BASE_URL = "http://localhost:8000"; //development
+export const BASE_URL = "https://api.poolpartyroorkee.in"; //production
